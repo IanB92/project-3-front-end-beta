159a8c8dc3edc1eb62c039598cbb3412b085957a
Ian <reddox215@gmail.com>
2022-04-08 16:02:08 -0400

    dfd

--- src/app/models/Instance.ts
+++ src/app/models/Instance.ts
@@ -1,5 +1,9 @@
 export class Instance {
+<<<<<<< HEAD
   // static url: string = "http://localhost:7777";
+=======
+  //static url: string = "http://localhost:7777";
+>>>>>>> parent of 147a971 (Merge pull request #14 from rebeccaAtRevature/main)
   static url: string = "http://ec2-34-230-18-27.compute-1.amazonaws.com:9999";
 }
 
